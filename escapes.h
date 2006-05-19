c4be2d2d861d5e96fcbbe9d198de991a397a252c
DaleStan <dalestan@gmail.com>
2006-05-19 18:11:56 +0000

    (svn r302) Write unquoted \7 and \D escape sequences.

---
+++ escapes.h
@@ -0,0 +1,103 @@
+// Escapes.h
+// The 
+
+
+#define START_ESCAPES()\
+	const struct esc{\
+		char byte;\
+		char*str;\
+		char action;\
+		uint pos;\
+		bool (*additional)(const U8*,uint);\
+		bool (*override)(const U8*,uint,uint);\
+	}escapes[]={
+
+#define ESCAPE(byte,str,action,off)\
+	{(char)0x##byte,(char*)("\\" str),(char)0x##action,off,NULL,NULL},
+
+#define ESCAPE_79(byte,str,off)\
+	ESCAPE(byte,str,7,off) ESCAPE(byte,str,9,off)
+
+#define ESCAPE_AD(byte,str,action,off,adtl)\
+	{(char)0x##byte,(char*)("\\" str),(char)0x##action,off,__ESC_AD__##adtl,NULL},
+
+#define ESCAPE_OVR(byte,str,action,ovr)\
+	{(char)0x##byte,(char*)("\\" str),(char)0x##action,0,NULL,__ESC_OVR__##ovr},
+
+#define END_ESCAPES() };\
+	static const int num_esc=sizeof(escapes)/sizeof(escapes[0]);
+
+#define CALLBACK_AD(name)\
+	bool __ESC_AD__##name(const U8*data,uint len)
+#define CALLBACK_OVR(name)\
+	bool __ESC_OVR__##name(const U8*data,uint len,uint pos)
+
+// ***********************************************************************
+// Define callback functions for ESCAPE_* here
+// Use the same value for the argument to the CALLBACK_* macro and the last
+// argument to ESCAPE_*
+// The signature for CALLBACK_AD is:
+// bool func(const U8*data, uint len)
+// The signature for CALLBACK_OVR is:
+// bool func(const U8*data, uint len, uint pos)
+// ***********************************************************************
+
+CALLBACK_AD(IsGRM){
+	return len==9&&data[2]==0&&data[4]==0xFE&&data[5]==0xFF;
+}
+
+
+START_ESCAPES()
+
+// ***********************************************************************
+// Define escape sequences here.
+// The arguments to ESCAPE* are:
+//   byte:   the byte value for this escape sequence, in hex
+//   str:    the string for this escape sequence, excluding the \
+//   action: the action for this escape sequence, eg 2, 7, D, &c.
+//           Note that 7 and 9 are usually paired, with ESCAPE_79, which does
+//           not take an action argument.
+//   off:    the offset in the action where this escape sequence appears
+// ESCAPE_AD takes an additional argument; the CALLBACK to call if the
+// byte, action, and off checks all pass.
+// EXCAPE_OVR takes an argument in place of off, the CALLBACK to call
+// instead of running the off check.
+// ***********************************************************************
+
+ESCAPE_79(00,"71",3)
+ESCAPE_79(01,"70",3)
+ESCAPE_79(02,"7=",3)
+ESCAPE_79(03,"7!",3)
+ESCAPE_79(04,"7<",3)
+ESCAPE_79(05,"7>",3)
+ESCAPE_79(06,"7G",3)
+ESCAPE_79(07,"7g",3)
+ESCAPE_79(08,"7gG",3)
+ESCAPE_79(09,"7GG",3)
+ESCAPE_79(0A,"7gg",3)
+ESCAPE_79(0B,"7c",3)
+ESCAPE_79(0C,"7C",3)
+
+ESCAPE(00,"D=",D,2)
+ESCAPE(01,"D+",D,2)
+ESCAPE(02,"D-",D,2)
+ESCAPE(03,"Du*",D,2)
+ESCAPE(04,"D*",D,2)
+ESCAPE(05,"Du<<",D,2)
+ESCAPE(06,"D<<",D,2)
+ESCAPE(07,"D&",D,2)
+ESCAPE(08,"D|",D,2)
+ESCAPE(09,"Du/",D,2)
+ESCAPE(0A,"D/",D,2)
+ESCAPE(0B,"Du%",D,2)
+ESCAPE(0C,"D%",D,2)
+
+ESCAPE_AD(00,"DR",D,3,IsGRM)
+ESCAPE_AD(01,"DF",D,3,IsGRM)
+ESCAPE_AD(02,"DC",D,3,IsGRM)
+ESCAPE_AD(03,"DM",D,3,IsGRM)
+ESCAPE_AD(04,"DnF",D,3,IsGRM)
+ESCAPE_AD(05,"DnC",D,3,IsGRM)
+ESCAPE_AD(06,"DO",D,3,IsGRM)
+
+END_ESCAPES()
