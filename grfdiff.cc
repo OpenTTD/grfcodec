c48a6a8c977a43520b0d02301e999ac3a4458d33
lakie <lakie@localhost>
2009-06-08 20:03:07 +0000

    (svn r2123) Updated to be what petern gave, after checking the function (argument is used as a "const char *" so it doesn't matter (fnsplit takes a "const char *" as its first argument)).

--- grfdiff.cc
+++ grfdiff.cc
@@ -253,7 +253,7 @@ void grfstore::getorgdata(U16 *size, U8 **data)
 	fseek(grf, fpos_cur, SEEK_SET);
 }
 
-char *basename(char *filename)
+char *basename(const char *filename)
 {
 	static char bname[32];
 	fnsplit(filename, NULL, NULL, bname, NULL);
@@ -389,7 +389,7 @@ int mkdiff()
 	}
 
 	if (grd) {
-		char *noext = basename((char *)grffile[1]);
+		char *noext = basename(grffile[1]);
 		char *p;
 
 		for (p = noext; *p; p++) { *p = tolower(*p); }
