788ff4fd94dfd85a297066b50912d3632e9eb115
DaleStan <DaleStan@localhost>
2009-06-18 15:58:56 +0000

    (svn r2124) Comments not prefixed by whitespace could cause NFORenum to reject the preceding escape. (Reported by DJ_Nekkid)

--- pseudo.cpp
+++ pseudo.cpp
@@ -647,7 +647,10 @@ uint PseudoSprite::ReadValue(istream& in, width w) {
 
 	// Read any other value
 	string str;
-	in>>str;
+	// can't use operator>> -- that will consume comments in cases like \w12000//comment
+	while(in && !is_comment(in) && !isspace(in.peek()) && in.peek() != EOF)
+		str += (char)in.get();
+
 	char c1, c2;
 	int y, m, d, count = sscanf(str.c_str(), "%d%c%d%c%d", &y, &c1, &m, &c2, &d);
 
