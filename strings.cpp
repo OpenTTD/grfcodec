d85b52c1d59d65ae2486292a2d1f3a52d74abca8
DaleStan <DaleStan@localhost>
2010-03-06 23:57:21 +0000

    (svn r2307) Fix, I think, issues with actions 1, 3, and 4 with an unknown feature. By Yexo: Add support for feature 10.

--- strings.cpp
+++ strings.cpp
@@ -62,7 +62,7 @@ void Check4(PseudoSprite&data){
 	data.SetAllHex();
 	uint i=5;
 	int nument=(signed)data.ExtractByte(3);
-	if(!IsValidFeature(ACT4,feature)&&feature!=0x48)IssueMessage(ERROR,INVALID_FEATURE);
+	if(!IsValidFeature(ACT4,feature)&&feature!=0x48){IssueMessage(FATAL,INVALID_FEATURE);return;}
 	if(_grfver<7&&lang&0x60&&(lang&0x7F)!=0x7F)IssueMessage(WARNING3,UNKNOWN_LANG_BIT,2,lang);
 	if(_grfver>6)CheckLangID(lang&0x7F,2);
 	if(lang&0x80){
