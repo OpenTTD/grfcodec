199a5da663053b6f479d4294facc76edaf8e3f74
Rubidium <rubidium@openttd.org>
2010-07-29 19:04:56 +0200

    Fix: "format not a string literal" warnings

--- grfcomm.h
+++ grfcomm.h
@@ -10,12 +10,6 @@ extern char *lastspritefilename;
 
 extern const char *e_openfile;
 
-void usage(const char *extratext
-#ifdef __cplusplus
-	 = ""
-#endif
-);
-
 char *spritefilename(const char *basefilename, const char *reldirectory, const char *ext, int spriteno, const char *mode, int mustexist);
 int doopen(const char *grffile, const char *dir, const char *ext, const char *mode,
 	char **filename, FILE **file, int mustexist);
