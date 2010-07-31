048b3b59734e74ffd2c6996539355a041a778e53
Rubidium <rubidium42@users.noreply.github.com>
2010-07-31 14:04:06 +0200

    Add: Checking of types and strings in an Action 14

--- act14.cpp
+++ act14.cpp
@@ -27,7 +27,53 @@ using namespace std;
 
 #include"renum.h"
 #include"pseudo.h"
+#include"messages.h"
+#include"strings.h"
+#include"command.h"
 
-void Check14(PseudoSprite&)
+static bool Check14(PseudoSprite&data, uint&offset)
 {
+	uint type = data.ExtractByte(offset++);
+	while (type != 0) {
+		uint id = data.ExtractDword(offset);
+		offset += 4;
+
+		switch (type) {
+			case 'C':
+				if (!Check14(data, offset)) return false;
+				break;
+
+			case 'T': {
+				uint langs = data.ExtractByte(offset);
+				extern uint _grfver;
+				/* Hardcode GRF version to 7 as that's needed for the language ids
+				 * and because we at least require version 7; that is actually
+				 * checked a little bit later during Action 8. */
+				_grfver = 7;
+				if (langs & 0x80) IssueMessage(WARNING2, UNKNOWN_LANG_BIT, offset, langs);
+				langs &= 0x7F;
+				CheckLangID(langs, offset++);
+				CheckString(data, offset, CTRL_COLOR | CTRL_NEWLINE);
+				break;
+			}
+
+			case 'B': {
+				uint size = data.ExtractWord(offset);
+				offset += 2 + size;
+				break;
+			}
+
+			default:
+				IssueMessage(FATAL, UNKNOWN_ACT14_TYPE, offset, type);
+				return false;
+		}
+		type = data.ExtractByte(offset++);
+	}
+	return true;
+}
+
+void Check14(PseudoSprite&data)
+{
+	uint offset = 1;
+	Check14(data, offset);
 }
