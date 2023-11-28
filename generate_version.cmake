831d0ae228bd53a9ff32744e67d85755eb1e6a3e
glx22 <glx@openttd.org>
2023-11-28 18:59:32 +0100

    Change: [CMake] Improve version detection

--- generate_version.cmake
+++ generate_version.cmake
@@ -1,18 +1,37 @@
-execute_process(
-	COMMAND ${GIT_EXECUTABLE} describe --tags --dirty=M
-	OUTPUT_VARIABLE GIT_VERSION
-	RESULT_VARIABLE GIT_DESCRIBE_RESULT
-	ERROR_VARIABLE GIT_DESCRIBE_ERROR
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+find_package(Git QUIET)
 
-execute_process(
-	COMMAND ${GIT_EXECUTABLE} show -s --pretty=%cd --date=short HEAD
-	OUTPUT_VARIABLE GIT_DATE
-	RESULT_VARIABLE GIT_SHOW_RESULT
-	ERROR_VARIABLE GIT_SHOW_ERROR
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+# .git may be a directory or a regular file
+if(GIT_FOUND AND EXISTS ".git")
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} describe --tags --always --dirty=M
+		OUTPUT_VARIABLE GIT_VERSION
+		RESULT_VARIABLE GIT_DESCRIBE_RESULT
+		ERROR_VARIABLE GIT_DESCRIBE_ERROR
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
 
-configure_file(${INPUT_FILE} ${OUTPUT_FILE})
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} show -s --pretty=%cd --date=short HEAD
+		OUTPUT_VARIABLE GIT_DATE
+		RESULT_VARIABLE GIT_SHOW_RESULT
+		ERROR_VARIABLE GIT_SHOW_ERROR
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+elseif(EXISTS ".version")
+	file(READ ".version" GIT_VERSION)
+	string(REPLACE "\n" "" GIT_VERSION "${GIT_VERSION}")
+	string(REPLACE "\t" ";" GIT_VERSION "${GIT_VERSION}")
+	list(GET GIT_VERSION 1 GIT_DATE)
+	list(GET GIT_VERSION 0 GIT_VERSION)
+else()
+	message(WARNING "No version detected")
+	set(GIT_VERSION "unknown")
+	set(GIT_DATE "unknown")
+endif()
 
+if(WRITE_VERSION)
+	file(WRITE .version "${GIT_VERSION}\t${GIT_DATE}\n")
+else()
+	message(STATUS "Generating version.h")
+	configure_file("src/version.h.in" "${GENERATED_BINARY_DIR}/version.h")
+endif()
